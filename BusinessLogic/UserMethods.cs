f8e96db3cb954cf813bf850371e0afefa5e0b67c
Mark Moore <mark.moore@revature.com>
2021-03-24 21:53:57 -0600

    test commit 1

--- BusinessLogic/UserMethods.cs
+++ BusinessLogic/UserMethods.cs
@@ -4,31 +4,42 @@ using Repository;
 
 namespace BusinessLogic
 {
-    public class UserMethods
-    {
-        private readonly MemeSaverRepo repolayer;
+	public class UserMethods
+	{
+		private readonly MemeSaverRepo repolayer;
 
-        public UserMethods(MemeSaverRepo repolayer)
-        {
-            this.repolayer = repolayer;
-        }
+		public UserMethods() { }
 
-        /// <summary>
-        /// This method takes a user and returns a verified user, if it exists.
-        /// </summary>
-        /// <param name="user"></param>
-        /// <returns></returns>
-        public Person Login(Person user)
-        {
-            //call a method on the repository class that will query the Db
-            // and return the verified person, if he exists.
-            // otherwise return a Person with empty strings.
-            user.Fname += user.Lname;
-            user.Lname += user.Fname;
+		public UserMethods(MemeSaverRepo repolayer)
+		{
+			this.repolayer = repolayer;
+		}
 
-            Person user1 = repolayer.Login(user);
+		public int QuadrupleTheInt(int x)
+		{
+			int y = 0;
+			y += x;
+			x += y;
+			x *= 2;
+			return x;
+		}
 
-            return user1;
-        }
-    }
+		/// <summary>
+		/// This method takes a user and returns a verified user, if it exists.
+		/// </summary>
+		/// <param name="user"></param>
+		/// <returns></returns>
+		public Person Login(Person user)
+		{
+			//call a method on the repository class that will query the Db
+			// and return the verified person, if he exists.
+			// otherwise return a Person with empty strings.
+			user.Fname += user.Lname;
+			user.Lname += user.Fname;
+
+			Person user1 = repolayer.Login(user);
+
+			return user1;
+		}
+	}
 }
---
+++ TestProject1/UnitTest1.cs
@@ -0,0 +1,23 @@
+using System;
+using BusinessLogic;
+using Xunit;
+
+namespace TestProject1
+{
+	public class UnitTest1
+	{
+		[Fact]
+		public void QuadrupleTheIntReturnsQuadrupleTheInt()
+		{
+			// arrange
+			var businessClass = new UserMethods();
+			int x = 4;
+
+			// act
+			int result = businessClass.QuadrupleTheInt(x);
+
+			// assert
+			Assert.Equal(16, result);
+		}
+	}
+}
